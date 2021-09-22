86b54798e4224c58a8fc79d4e910ea4753e133ec
Usuario <201600853@ujmd.edu.sv>
2021-09-21 20:56:36 -0600

    PRIMER EJERCICO TERMINADO1

--- cuadratica/src/Cuadratica/Cuadratica.java
+++ cuadratica/src/Cuadratica/Cuadratica.java
@@ -23,16 +23,16 @@ public class Cuadratica {
     }
 
     public Cuadratica() {
-        a = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el valor de a: "));
-        b = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el valor de b: "));
-        c = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el valor de c: "));
+        a = Integer.parseInt(JOptionPane.showInputDialog("Ingrese A: "));
+        b = Integer.parseInt(JOptionPane.showInputDialog("Ingrese B: "));
+        c = Integer.parseInt(JOptionPane.showInputDialog("Ingrese C: "));
         double determinante = Math.pow(b, 2) - (4 * a * c);
         if (determinante > 0) {
             double x1 = ((b * (-1)) + Math.sqrt(determinante)) / (2 * a);
             double x2 = ((b * (-1)) - Math.sqrt(determinante)) / (2 * a);
             System.out.println("El valor de x1 = " + x1 + " y el valor de x2= " + x2);
         } else {
-            System.out.println("El determinante es Negativo, no se puede completar la operacion");
+            System.out.println("El determinante es Negativo,no se podra realizar la operacion");
         }
     }
 }
